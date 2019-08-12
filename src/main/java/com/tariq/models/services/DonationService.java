ef59976079221ba704d6c1d56d57f0921dfdb255
Uzair Tariq <tariquu@hendrix.edu>
2019-08-12 15:12:53 -0400

    Services fix
    Services fixed with the right imports and implemented methods

--- src/main/java/com/tariq/models/services/DonationService.java
+++ src/main/java/com/tariq/models/services/DonationService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ketu.model.beans.Donation;
 // Import Donation Bean
 //
 import com.tariq.models.repos.DonationRepo;
@@ -12,6 +14,81 @@ public class DonationService implements DonationRepo {
 	
 	@Autowired
 	private DonationRepo dr;
+
+	@Override
+	public <S extends Donation> S save(S entity) {
+		// TODO Auto-generated method stub
+		return dr.save(entity);
+	}
+
+	@Override
+	public <S extends Donation> Iterable<S> saveAll(Iterable<S> entities) {
+		// TODO Auto-generated method stub
+		return dr.saveAll(entities);
+	}
+
+	@Override
+	public Optional<Donation> findById(Long id) {
+		// TODO Auto-generated method stub
+		return dr.findById(id);
+	}
+
+	@Override
+	public boolean existsById(Long id) {
+		// TODO Auto-generated method stub
+		return dr.existsById(id);
+	}
+
+	@Override
+	public Iterable<Donation> findAllById(Iterable<Long> ids) {
+		// TODO Auto-generated method stub
+		return dr.findAllById(ids);
+	}
+
+	@Override
+	public long count() {
+		// TODO Auto-generated method stub
+		return dr.count();
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		// TODO Auto-generated method stub
+		dr.deleteById(id);
+	}
+
+	@Override
+	public void delete(Donation entity) {
+		// TODO Auto-generated method stub
+		dr.delete(entity);
+		
+	}
+
+	@Override
+	public void deleteAll(Iterable<? extends Donation> entities) {
+		// TODO Auto-generated method stub
+		dr.deleteAll(entities);
+		
+	}
+
+	@Override
+	public void deleteAll() {
+		// TODO Auto-generated method stub
+		dr.deleteAll();
+		
+	}
+
+	@Override
+	public Donation findById(long id) {
+		// TODO Auto-generated method stub
+		return dr.findById(id);
+	}
+
+	@Override
+	public List<Donation> findAll() {
+		// TODO Auto-generated method stub
+		return dr.findAll();
+	}
 
 
 }
--- src/main/java/com/tariq/models/services/OrderService.java
+++ src/main/java/com/tariq/models/services/OrderService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ketu.model.beans.Order;
 // Import Donation Bean
 //
 import com.tariq.models.repos.OrderRepo;
@@ -12,6 +14,82 @@ public class OrderService implements OrderRepo {
 	
 	@Autowired
 	private OrderRepo or;
+
+	@Override
+	public <S extends Order> S save(S entity) {
+		// TODO Auto-generated method stub
+		return or.save(entity);
+	}
+
+	@Override
+	public <S extends Order> Iterable<S> saveAll(Iterable<S> entities) {
+		// TODO Auto-generated method stub
+		return or.saveAll(entities);
+	}
+
+	@Override
+	public Optional<Order> findById(Long id) {
+		// TODO Auto-generated method stub
+		return or.findById(id);
+	}
+
+	@Override
+	public boolean existsById(Long id) {
+		// TODO Auto-generated method stub
+		return or.existsById(id);
+	}
+
+	@Override
+	public Iterable<Order> findAllById(Iterable<Long> ids) {
+		// TODO Auto-generated method stub
+		return or.findAllById(ids);
+	}
+
+	@Override
+	public long count() {
+		// TODO Auto-generated method stub
+		return or.count();
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		// TODO Auto-generated method stub
+		or.deleteById(id);
+		
+	}
+
+	@Override
+	public void delete(Order entity) {
+		// TODO Auto-generated method stub
+		or.delete(entity);;
+		
+	}
+
+	@Override
+	public void deleteAll(Iterable<? extends Order> entities) {
+		// TODO Auto-generated method stub
+		or.deleteAll(entities);
+		
+	}
+
+	@Override
+	public void deleteAll() {
+		// TODO Auto-generated method stub
+		or.deleteAll();
+		
+	}
+
+	@Override
+	public Order findById(long id) {
+		// TODO Auto-generated method stub
+		return or.findById(id);
+	}
+
+	@Override
+	public List<Order> findAll() {
+		// TODO Auto-generated method stub
+		return or.findAll();
+	}
 
 
 }
