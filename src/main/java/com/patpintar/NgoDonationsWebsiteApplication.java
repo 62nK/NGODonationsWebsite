8ce8420c493ff7d83e36cb7cedcba4f89a31610b
Andrea Pinardi <andreapinardi39@gmail.com>
2019-08-12 10:59:47 -0400

    Initial Commit

---
+++ src/main/java/com/patpintar/NgoDonationsWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.patpintar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgoDonationsWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgoDonationsWebsiteApplication.class, args);
+	}
+
+}
